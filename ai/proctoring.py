e953abebb9323477048adbf86a1ce5d8d07b187b
Rodion Smolyar <s1rodion123@gmail.com>
2025-09-01 22:25:12 +0300

    added Proctoring class that checks if a person is using a mobile phone or not in a frame

--- ai/proctoring.py
+++ ai/proctoring.py
@@ -1,43 +1,1 @@
-from enum import Enum
-
-from PIL import Image
-
-
-class SuspicionLevel(Enum):
-    """Enum that describes proctor concerns about interviewee."""
-
-    NORMAL = 0
-    SLIGHTLY_SUSPICIOUS = 1
-    SUSPICIOUS = 2
-    VERY_SUSPICIOUS = 3
-
-
-class Proctor:
-    """
-    Proctor for the interview that is trained to detect anomalies of interviewee behaviour and report it.
-
-    Proctor implements something like clever state machine,
-    which is able to distinguish one-off suspicious movement from consistent.
-    For example, if person only ever does something suspicion and
-    after than system is not detecting any anomalies for a long time,
-    then suspicion level is dropped.
-    Something similar also happens if person frequently behaves suspicious.
-    """
-
-    def __init__(self, pretrained: bool = True):
-        self.last_suspicious_timecode = None
-        self.suspicion_level = SuspicionLevel.NORMAL
-
-        self.model = None  # https://pypi.org/project/proctoring/
-
-    def analyze(self, image: Image, timecode: int) -> SuspicionLevel:
-        """
-        Analyzes image of the interviewee and detects anomalies of his behaviour.
-
-        :param image: Image of the interviewee
-        :param timecode: Timecode of the image
-
-        :returns: Current concerns of the proctor
-        """
-
-        return self.suspicion_level
+from ultralytics import YOLOclass Proctoring:    """    Checks if a person uses mobile phone or is not in the frame    """    def __init__(self, path):        self.model = YOLO('yolov8x.pt')        self.path = path    def is_cheating(self) -> None:        """        Prints if a person is cheating        """        results = self.model(self.path, conf=0.1)        person_detected = False        phone_detected = False        for result in results:            for box in result.boxes:                cls = int(box.cls[0])                label = result.names[cls]                if label == 'person':                    person_detected = True                elif label == 'cell phone':                    phone_detected = True        if not person_detected:            print("Нет человека на изображении!")        if person_detected and phone_detected:            print("Телефон в руках обнаружен!")        else:            print("Всё в порядке.")if __name__ == "__main__":    # testing    # pr = Proctoring("fjsdkljflksd.png")    # pr.is_cheating()    pass
