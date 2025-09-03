7d4d8368240a8a5593a0d221ef68e69a5706bd80
JktuJQ <odmamontov@gmail.com>
2025-09-03 21:59:02 +0300

    'tests' -> 'examples' folder rename

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--test", dest="test", type=str, required=False,
+    parser.add_argument("--example", dest="example", type=str, required=False,
                         help="Run file for testing purposes")
     args = parser.parse_args()
 
-    if args.test is not None:
+    if args.example is not None:
         try:
-            exec(f"import tests.{args.test}")
+            exec(f"import examples.{args.example}")
         except ImportError:
-            print(f"There is no {args.test} test file")
+            print(f"There is no `examples/{args.example}.py` test file")
+        except Exception as e:
+            print(e)
         finally:
             exit()
     else:
