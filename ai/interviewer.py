0fa2cf74a95d9bc52f97fea948cdb62d398ea61e
Rodion Smolyar <s1rodion123@gmail.com>
2025-09-06 19:39:15 +0300

    added a function that converts speech to text

--- ai/interviewer.py
+++ ai/interviewer.py
@@ -1,4 +1,7 @@
 from globals import *
+from gtts import gTTS
+import pygame
+import io
 
 
 class Interviewer:
@@ -22,6 +25,21 @@ class Interviewer:
         self.text_to_speech = None  # https://github.com/KoljaB/RealtimeTTS
         # https://github.com/KoljaB/RealtimeSTT/blob/master/tests/minimalistic_talkbot.py
 
+    @staticmethod
+    def text_to_speech_online(text):
+        """Converts speech to text in Russian"""
+        tts = gTTS(text=text, lang='ru')
+        fp = io.BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(fp)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
     # Still need to think about the API, but it seems that realtimedness complicates things a LOT.
     def process_text(text):
         print(text)
+
